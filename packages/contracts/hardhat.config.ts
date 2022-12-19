bcc446459fc1352cd6af6dd8c590a35412d44580
tema <artem.rusanov.02@gmail.com>
2022-12-19 18:33:00 +0700

    feat: add contract package

---
+++ packages/contracts/hardhat.config.ts
@@ -0,0 +1,94 @@
+import { get } from 'env-var'
+import { HardhatUserConfig } from "hardhat/types"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-deploy"
+import "@typechain/hardhat"
+import "hardhat-abi-exporter"
+import "@nomiclabs/hardhat-etherscan"
+import "@openzeppelin/hardhat-upgrades"
+import "solidity-coverage"
+import "hardhat-gas-reporter"
+import "./tasks"
+
+const config: HardhatUserConfig = {
+    defaultNetwork: "hardhat",
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.12",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
+    },
+    paths: {
+        tests: "./tests",
+        artifacts: "./build/artifacts",
+        cache: "./build/cache",
+        deployments: "./build/deployments",
+    },
+    typechain: {
+        outDir: "./sdk",
+    },
+    gasReporter: {
+        currency: "USD",
+        coinmarketcap: get('COINMARKETCAP_API_KEY').asString(),
+        token: get('TOKEN').asString(),
+        gasPriceApi: get('GAS_PRICE_API').asString(),
+        enabled: get('REPORT_GAS').asBool(),
+        maxMethodDiff: 10,
+    },
+    networks: {
+        hardhat: {},
+        localhost: {
+            url: "http://127.0.0.1:8545",
+        },
+        ethereum: {
+            url: get('NODE_URL').required().asUrlString(),
+            accounts: {
+                mnemonic: get('MNEMONIC').required().asString(),
+            },
+            chainId: 1,
+        },
+        goerli: {
+            url: get('NODE_URL').required().asUrlString(),
+            accounts: {
+                mnemonic: get('MNEMONIC').required().asString(),
+            },
+        },
+        avalanche: {
+            url: get('NODE_URL').default("https://api.avax.network/ext/bc/C/rpc").required().asUrlString(),
+            accounts: {
+                mnemonic: get('MNEMONIC').required().asString(),
+            },
+        },
+        fuji: {
+            url: get('NODE_URL').default("https://api.avax-test.network/ext/bc/C/rpc").required().asUrlString(),
+            accounts: {
+                mnemonic: get('MNEMONIC').required().asString(),
+            },
+        },
+    },
+    namedAccounts: {
+        deployer: 0,
+    },
+    etherscan: {
+        apiKey: get('ETHERSCAN_API_KEY').default("API_KEY_WEB").asString(),
+    },
+    abiExporter: {
+        path: "./build/abis",
+        runOnCompile: true,
+        clear: true,
+        flat: true,
+        only: [],
+        spacing: 2,
+        pretty: false,
+    },
+}
+
+export default config
